0fee4f5db4a5b082c1bdd5edd65d895db41a13a9
kang <xiyoulinux.kangyijie@gmail.com>
2017-05-24 21:30:19 +0800

    添加注释

--- auto-recovery.py
+++ auto-recovery.py
@@ -5,16 +5,17 @@ import os
 import time
 import sys
 
-# 检测是否存活
+# 检测port对应节点是否存活
 def check(port):
     isAlive = "ps -ef | grep \"" + port + " \[cluster\]\"" 
     recovery = "redis-server ./" + port + "/redis.conf"  
-    if os.system(isAlive): #判断
-        os.system(recovery) #重启节点
+    if os.system(isAlive):  #判断
+        os.system(recovery) #进程不存在，重启节点
 
 
 
-#设置为守护进程
+#设置为守护进程 
+
 stdin = '/dev/null'
 stdout = '/dev/null'
 stderr = '/dev/null'
@@ -24,24 +25,28 @@ try:
     if pid > 0 :
         sys.exit(0)
 except OSError, e:
-    #sys.stderr.write("Fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))
+    #sys.stderr.write("Fork #1 failed: (%d) %s\n" % (e.errno, e.strerror)) 这句话没看懂
     print "error #1"
     sys.exit(1)
 
-os.chdir("./redis_cluster")
-os.umask(0)
-os.setsid()
+os.chdir("./redis_cluster") # 切换工作目录
+os.umask(0)                 # 设置文件模式创建屏蔽字
+os.setsid()                 # 甩掉控制终端
 
+# 第二次fork 保证子进程不是会话首进程
 try:
     pid = os.fork()
     if pid > 0:
-        sys.exit(0)
+        sys.exit(0)        #父进程退出
 except OSError:
     #sys.stderr.write("Fork #2 failed:(%d) %s\n" % (e.errno, e.strerror))
     print "error #2"
     sys.exit(1)
 
-#for f in sys.stdout, sys.stderr: f.flush() 这句话没弄明白
+for f in sys.stdout, sys.stderr: 
+	f.flush()              # 刷新缓冲区
+	
+#将标准输出输入错误改为/dev/null 	
 si = open(stdin, 'r')
 so = open(stdout, 'a+')
 se = open(stderr, 'a+', 0)
@@ -49,7 +54,7 @@ os.dup2(si.fileno(), sys.stdin.fileno())
 os.dup2(so.fileno(), sys.stdout.fileno())
 os.dup2(se.fileno(), sys.stderr.fileno())
 
-#循环 每5s检测是存活
+#主循环 每5s检测port进程是否存活
 port = sys.argv[1]  
 while True:
     check(port)
